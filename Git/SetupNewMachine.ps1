d195f258c36a93e4e08b29a5598939432cb80764
rdoconnor <ryan.d.oconnor.14@gmail.com>
2020-06-15 15:36:09 -0700

    Adding new machine git setup script

---
+++ Git/SetupNewMachine.ps1
@@ -0,0 +1,10 @@
+$notepadExe = 'C:/Program Files/Notepad++/notepad++.exe'
+
+if (Test-Path $notepadExe)
+{
+	git config --global core.editor "$notepadExe -multiInst -notabbar -nosession -noPlugin"
+}
+else
+{
+	Write-Host "notepad++ doesn't exist"
+}
