62c57b0fb71d388e932f62d6d2de27ebfb6d136b
rdoconnor <ryan.d.oconnor.14@gmail.com>
2020-06-15 15:36:37 -0700

    Updating new machine git setup script

--- Git/SetupNewMachine.ps1
+++ Git/SetupNewMachine.ps1
@@ -2,7 +2,7 @@ $notepadExe = 'C:/Program Files/Notepad++/notepad++.exe'
 
 if (Test-Path $notepadExe)
 {
-	git config --global core.editor "$notepadExe -multiInst -notabbar -nosession -noPlugin"
+	git config --global core.editor "'$notepadExe' -multiInst -notabbar -nosession -noPlugin"
 }
 else
 {
